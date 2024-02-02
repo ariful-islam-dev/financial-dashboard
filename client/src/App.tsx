bb419e354081ff193425530cdb3427bc655ab257
ariful4082 <ariful4082@gmail.com>
2024-02-03 01:02:34 +0600

    Dashboard design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./scenes/navbar"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import Dashboard from "@/scenes/dashboard"
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className='app'>
-     
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/predictions" element={<div>Predictions page</div>}/>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
