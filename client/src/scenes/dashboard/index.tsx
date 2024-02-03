3de507817accc092a0fa5ddaed22e8f5c93c010f
ariful4082 <ariful4082@gmail.com>
2024-02-03 22:50:20 +0600

    dashboard component design done without data

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,7 +1,8 @@
+import { Box, useMediaQuery } from '@mui/material'
+import Row1 from './Row1';
+import Row2 from './Row2';
+import Row3 from './Row3';
 
-import { Box, useMediaQuery, useTheme } from '@mui/material'
-
-type Props = {}
 
 const gridTemplateLargeScreens = `
     "a b c"
@@ -47,35 +48,29 @@ const gridTemplateSmallScreens = `
     "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
-    const { palette } = useTheme();
+
     return (
         <Box
             width="100%"
             height="100%"
             display="grid"
             gap="1.5rem"
-            sx={isAboveMediumScreens?{
+            sx={isAboveMediumScreens ? {
                 gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
                 gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
                 gridTemplateAreas: gridTemplateLargeScreens
-            }:{
+            } : {
                 gridAutoColumns: "1fr",
                 gridAutoRows: "80px",
                 gridTemplateAreas: gridTemplateSmallScreens
             }}
         >
-            <Box bgcolor="#fff" gridArea="a"></Box>
-            <Box bgcolor="#fff" gridArea="b"></Box>
-            <Box bgcolor="#fff" gridArea="c"></Box>
-            <Box bgcolor="#fff" gridArea="d"></Box>
-            <Box bgcolor="#fff" gridArea="e"></Box>
-            <Box bgcolor="#fff" gridArea="f"></Box>
-            <Box bgcolor="#fff" gridArea="g"></Box>
-            <Box bgcolor="#fff" gridArea="h"></Box>
-            <Box bgcolor="#fff" gridArea="i"></Box>
-            <Box bgcolor="#fff" gridArea="j"></Box>
+
+            <Row1 />
+            <Row2 />
+            <Row3/>
         </Box>
     )
 }
