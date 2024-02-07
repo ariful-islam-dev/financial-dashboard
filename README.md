20d708f2a69347b7ce3d55a2b1c1463c5e48f676
ariful4082 <ariful4082@gmail.com>
2024-02-07 19:57:14 +0600

    complete the 1st row

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Financial Dashboard Application
 <p>Fullstack Web Application</p>
+
+## Dashboard
+- [x] AreaChart
+- [x] LineChart
+- [x] BarChart
+  
+
+## How to run this project
+1. `Run` your database local (for local)
+2. open terminal
+3. Go to server directory command `cd server`
+4. Create `.env` file like `example.env` (for server)
+5. Fill the `.env` file condition like `variable, link or name`
+6. `Run` the server to using this command `yarn dev`
+7. Go to Client directory command `cd client`
+8. Create `.env` file like `example.env` (for frontend)
+9. Fill the `.env` file condition like `variable, link or name`
+10. `Run` the server to using this command `yarn dev`
--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -2,50 +2,58 @@ import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
 import { useTheme } from '@mui/material';
 import { useMemo } from 'react';
-import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import BoxHeader from './BoxHeader';
 
-type Props = {}
+const Row1 = () => {
 
-const Row1 = (props: Props) => {
-
-    const {palette}=useTheme();
+    const { palette } = useTheme();
     const { data } = useGetKpisQuery()
-        
-    const revenueExpenses = useMemo(()=> {
+
+    const revenueExpenses = useMemo(() => {
         return (
-            data && data[0].monthlyData.map(({month, revenue, expenses})=>{
+            data && data[0].monthlyData.map(({ month, revenue, expenses }) => {
                 return {
-                    name: month.substring(0,3),
-                    revenue:  revenue,
+                    name: month.substring(0, 3),
+                    revenue: revenue,
                     expenses: expenses
                 }
             })
         )
-    }, 
-    [data]);
-     // console.log("data:", data);
-    const revenueProfit = useMemo(()=> {
+    },
+        [data]);
+    // For Profit and Revenue Line Chart
+    const revenueProfit = useMemo(() => {
         return (
-            data && data[0].monthlyData.map(({month, revenue, expenses})=>{
+            data && data[0].monthlyData.map(({ month, revenue, expenses }) => {
                 return {
-                    name: month.substring(0,3),
-                    revenue:  revenue,
+                    name: month.substring(0, 3),
+                    revenue: revenue,
                     profit: (revenue - expenses).toFixed(2)
                 }
             })
         )
-    }, 
-    [data]);
-   
+    }, [data]); 
+    // for Revenue Bar Chart
+    const revenue = useMemo(() => {
+        return (
+            data && data[0].monthlyData.map(({ month, revenue }) => {
+                return {
+                    name: month.substring(0, 3),
+                    revenue: revenue
+                }
+            })
+        )
+    }, [data]);
 
-    
+
+
     return (
         <>
             <DashboardBox gridArea="a">
                 <BoxHeader title="Revenue and Expenses"
-                subtitle="top line represents revenue, bottom line represents expenses"
-                sideText="+4%"
+                    subtitle="top line represents revenue, bottom line represents expenses"
+                    sideText="+4%"
                 />
                 <ResponsiveContainer width="100%" height="100%">
                     <AreaChart
@@ -55,32 +63,32 @@ const Row1 = (props: Props) => {
                         margin={{
                             top: 15,
                             right: 25,
-                            left:-10,
+                            left: -10,
                             bottom: 60,
                         }}
                     >
                         <defs>
 
-                        <linearGradient id='colorRevenue' x1='0' y1="0" x2="0" y2="1">
-                            <stop offset={"5%"} stopColor={palette.primary[300]} stopOpacity={0.5}/>
-                            <stop offset={"95%"} stopColor={palette.primary[300]} stopOpacity={0}/>
-                        </linearGradient>
-                        <linearGradient id='colorExpenses' x1='0' y1="0" x2="0" y2="1">
-                            <stop offset={"5%"} stopColor={palette.primary[300]} stopOpacity={0.5}/>
-                            <stop offset={"95%"} stopColor={palette.primary[300]} stopOpacity={0}/>
-                        </linearGradient>
+                            <linearGradient id='colorRevenue' x1='0' y1="0" x2="0" y2="1">
+                                <stop offset={"5%"} stopColor={palette.primary[300]} stopOpacity={0.5} />
+                                <stop offset={"95%"} stopColor={palette.primary[300]} stopOpacity={0} />
+                            </linearGradient>
+                            <linearGradient id='colorExpenses' x1='0' y1="0" x2="0" y2="1">
+                                <stop offset={"5%"} stopColor={palette.primary[300]} stopOpacity={0.5} />
+                                <stop offset={"95%"} stopColor={palette.primary[300]} stopOpacity={0} />
+                            </linearGradient>
                         </defs>
-                        <XAxis dataKey="name" tickLine={false} style={{fontSize:'10px'}} />
-                        <YAxis tickLine={false} style={{fontSize:'10px'}}  axisLine={{strokeWidth: "0"}} domain={[8000, 23000]}/>
+                        <XAxis dataKey="name" tickLine={false} style={{ fontSize: '10px' }} />
+                        <YAxis tickLine={false} style={{ fontSize: '10px' }} axisLine={{ strokeWidth: "0" }} domain={[8000, 23000]} />
                         <Tooltip />
-                        <Area 
+                        <Area
                             type={"monotone"}
                             dataKey={"revenue"}
                             stroke={palette.primary.main}
                             fillOpacity={1}
                             dot={true}
                             fill='url(#colorRevenue)'
-                        /><Area 
+                        /><Area
                             type={"monotone"}
                             dataKey={"expenses"}
                             stroke={palette.primary.main}
@@ -92,9 +100,9 @@ const Row1 = (props: Props) => {
                 </ResponsiveContainer>
             </DashboardBox>
             <DashboardBox gridArea="b">
-            <BoxHeader title="Profit and Revenue"
-                subtitle="top line represents revenue, bottom line represents expenses"
-                sideText="+4%"
+                <BoxHeader title="Profit and Revenue"
+                    subtitle="top line represents revenue, bottom line represents expenses"
+                    sideText="+4%"
                 />
                 <ResponsiveContainer width="100%" height="100%">
                     <LineChart
@@ -104,46 +112,85 @@ const Row1 = (props: Props) => {
                         margin={{
                             top: 20,
                             right: 0,
-                            left:-10,
+                            left: -10,
                             bottom: 55,
                         }}
                     >
-                       <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
-                        <XAxis 
-                        dataKey="name"
-                        tickLine={false} 
-                        style={{fontSize:'10px'}}
-                         />
-                        <YAxis 
-                        yAxisId={"left"} 
-                        tickLine={false} 
-                        style={{fontSize:'10px'}}  
-                        axisLine={false} 
+                        <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+                        <XAxis
+                            dataKey="name"
+                            tickLine={false}
+                            style={{ fontSize: '10px' }}
                         />
-                        <YAxis 
-                        yAxisId={"right"} 
-                        orientation='right' 
-                        tickLine={false} 
-                        axisLine={false}
-                        style={{fontSize:'10px'}}  
-                         />
+                        <YAxis
+                            yAxisId={"left"}
+                            tickLine={false}
+                            style={{ fontSize: '10px' }}
+                            axisLine={false}
+                        />
+                        <YAxis
+                            yAxisId={"right"}
+                            orientation='right'
+                            tickLine={false}
+                            axisLine={false}
+                            style={{ fontSize: '10px' }}
+                        />
                         <Tooltip />
-                        <Legend height={20} wrapperStyle={{margin: "0 0 10px 0"}}/>
+                        <Legend height={20} wrapperStyle={{ margin: "0 0 10px 0" }} />
                         <Line
                             yAxisId={"left"}
                             type={"monotone"}
                             dataKey={"profit"}
                             stroke={palette.tertiary[500]}
-                        /><Line 
-                        yAxisId={"right"}
-                        type={"monotone"}
-                        dataKey={"revenue"}
-                        stroke={palette.primary.main}
+                        /><Line
+                            yAxisId={"right"}
+                            type={"monotone"}
+                            dataKey={"revenue"}
+                            stroke={palette.primary.main}
                         />
                     </LineChart>
                 </ResponsiveContainer>
             </DashboardBox>
-            <DashboardBox gridArea="c"></DashboardBox>
+            <DashboardBox gridArea="c">
+            <BoxHeader title="Revenue Month by Month"
+                    subtitle="top line represents revenue, bottom line represents expenses"
+                    sideText="+4%"
+                />
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={revenue}
+                        margin={{
+                            top: 17,
+                            right: 15,
+                            left: -5,
+                            bottom: 58,
+                        }}
+                    >
+                        <defs>
+                            <linearGradient id='colorRevenue' x1='0' y1="0" x2="0" y2="1">
+                                <stop offset={"5%"} stopColor={palette.primary[300]} stopOpacity={0.8} />
+                                <stop offset={"95%"} stopColor={palette.primary[300]} stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+                        <XAxis
+                            dataKey="name"
+                            axisLine={false}
+                            tickLine={false}
+                            style={{ fontSize: '10px' }}
+                        />
+                        <YAxis
+                            axisLine={false}
+                            tickLine={false}
+                            style={{ fontSize: '10px' }}
+                        />
+                        <Tooltip />
+                        <Bar dataKey="revenue" fill="url(#colorRevenue)" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </DashboardBox>
         </>
     )
 }
